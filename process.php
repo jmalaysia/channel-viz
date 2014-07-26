b474c4017cd9779d77d073c67b19579a78006ff2
jmalaysia <jmalaysia@users.noreply.github.com>
2014-07-25 21:56:12 -0500

    Create process.php

---
+++ process.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html/>
+
+<html>
+    
+	<head> 
+		<title>Underwood Oil</title>
+		<link rel="stylesheet" type "text/css" href="style.css" />
+	</head>
+	
+	<body>
+		<div id = "container">
+			<div id = "header">
+				<h1> Underwood Oil Dipper </h1>
+			</div>
+			<div id = "content">
+				<div id = "nav">
+				<h3> FIELDS </h3>
+					<ul>
+						<li> <a class="selected" href="well1"> Underwood A </a></li>
+						<li> <a class="selected" href="well2"> Underwood </a></li>
+						<li> <a class="selected" href="well3"> Bert Poteet </a></li>
+						<li> <a class="selected" href="well4"> Houchens </a></li>
+					</ul>
+				</div>
+				<div id = "fault">
+				<h3> Well Status </h3>
+                    <?php
+                        echo "Operational";
+                    ?>
+				</div>
+				<div id = "level">
+				<h3> Tank Battery Volume </h3>
+                    <?php
+                        echo "74 Gallons";
+                    ?>
+				</div>
+				<div id = "config">
+				<h3> Well Configuration </h3>
+                    <?php
+                        echo "Click Here to Change Parameters";
+                    ?>
+				</div>
+			</div>
+				<div id = "footer">
+				<h3> Copyright &copy; 2014 John Hudson </h3>
+                <h3>hudsoncontrols@gmail.com</h3>
+				</div>
+		</div>
+	</body>
+    
+</html>
